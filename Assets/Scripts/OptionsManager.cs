fa65d140636c49421251a1cd4330260e077e7b1d
OscF95 <ofariasr1@gmail.com>
2023-05-01 22:41:59 -0500

    Resolver bug null reference exception

--- Assets/Scripts/OptionsManager.cs
+++ Assets/Scripts/OptionsManager.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 public class OptionsManager : MonoBehaviour
 {
     private Toggle soundToggle;
+    private GameObject toggleObject;
     public static bool isSoundActive = true;
 
     private void Start()
     {
-        GameObject objeto = GameObject.FindWithTag("SoundToggle");
-        soundToggle = objeto.GetComponent<Toggle>();
-        soundToggle.isOn = isSoundActive;
-        AudioListener.volume = isSoundActive ? 1 : 0;
-        Debug.Log(soundToggle.isOn);
+        toggleObject = GameObject.FindWithTag("SoundToggle");
+        if (toggleObject) 
+        {
+            soundToggle = toggleObject.GetComponent<Toggle>();
+            soundToggle.isOn = isSoundActive;
+            AudioListener.volume = isSoundActive ? 1 : 0;
+        }
     }
 
     public void ChangeSoundStatus(bool state)
